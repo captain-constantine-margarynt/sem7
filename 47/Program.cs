6ed8eb5f61849c241cf195ad629f1e27b589291f
captain-constantine-margarynt <89223974+captain-constantine-margarynt@users.noreply.github.com>
2023-06-22 13:43:24 +0300

    HW7. Task 47

---
+++ 47/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+double [,] array = GetArray(rows, columns);
+Write("Original array: ");
+WriteLine();
+PrintArray(array);
+WriteLine();
+
+
+
+
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = rand.NextDouble();
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
